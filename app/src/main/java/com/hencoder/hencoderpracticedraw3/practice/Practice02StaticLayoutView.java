4a2d7d4c24ead02d8fcf52b99da41dfaa1223e9e
StephenZKCurry <2029386632@qq.com>
2019-09-19 16:45:56 +0800

    完成练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice02StaticLayoutView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice02StaticLayoutView.java
@@ -2,8 +2,11 @@ package com.hencoder.hencoderpracticedraw3.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -34,6 +37,19 @@ public class Practice02StaticLayoutView extends View {
 
         // 使用 StaticLayout 代替 Canvas.drawText() 来绘制文字，
         // 以绘制出带有换行的文字
-        canvas.drawText(text, 50, 100, textPaint);
+//        canvas.drawText(text, 50, 100, textPaint);
+        canvas.save();
+        canvas.translate(50, 50);
+        StaticLayout staticLayout = new StaticLayout(text, textPaint, 600,
+                Layout.Alignment.ALIGN_NORMAL, 1, 0, true);
+        staticLayout.draw(canvas);
+        canvas.restore();
+
+        canvas.save();
+        canvas.translate(50, 200);
+        staticLayout = new StaticLayout("在Android开发中，Canvas.drawText不会换行，即使一个很长的字符串也只会显示一行，超出部分会隐藏在屏幕之外.StaticLayout是android中处理文字的一个工具类，StaticLayout 处理了文字换行的问题", textPaint, 600,
+                Layout.Alignment.ALIGN_NORMAL, 1, 0, true);
+        staticLayout.draw(canvas);
+        canvas.restore();
     }
 }
